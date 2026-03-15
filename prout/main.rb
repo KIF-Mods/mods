47f87c9270be13a46f251b065bbb4d7490f433fb
sKarreku <petronijevicalekss@gmail.com>
2026-03-15 20:25:44 +0100

    Update prout

---
+++ prout/main.rb
@@ -0,0 +1,15 @@
+#==============================================================================
+# Prout
+# by Me — v1.0.0
+#
+# proutor
+#==============================================================================
+
+# Your mod code here.
+# Access your mod's settings via:
+#   $mod_manager_settings["prout"]
+#
+# Example:
+#   if $mod_manager_settings["prout"]["my_setting"]
+#     # do something
+#   end
