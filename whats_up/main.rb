5f1c559bdebbbbb2270b795a0400dfb008d94500
sKarreku <petronijevicalekss@gmail.com>
2026-03-15 20:48:05 +0100

    Update whats_up

---
+++ whats_up/main.rb
@@ -0,0 +1,15 @@
+#==============================================================================
+# whats up
+# by Cody — v1.0.0
+#
+# Im not cody
+#==============================================================================
+
+# Your mod code here.
+# Access your mod's settings via:
+#   $mod_manager_settings["whats_up"]
+#
+# Example:
+#   if $mod_manager_settings["whats_up"]["my_setting"]
+#     # do something
+#   end
