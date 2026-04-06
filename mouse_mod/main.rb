7f2594cf2293c9d4aa877d2f4aa21a1219478fba
John <na@na>
2026-04-06 11:50:02 -0400

    Update mouse_mod

---
+++ mouse_mod/main.rb
@@ -0,0 +1,15 @@
+#==============================================================================
+# Mouse Mod
+# by Probably — v1.0.0
+#
+# lets you click things
+#==============================================================================
+
+# Your mod code here.
+# Access your mod's settings via:
+#   $mod_manager_settings["mouse_mod"]
+#
+# Example:
+#   if $mod_manager_settings["mouse_mod"]["my_setting"]
+#     # do something
+#   end
